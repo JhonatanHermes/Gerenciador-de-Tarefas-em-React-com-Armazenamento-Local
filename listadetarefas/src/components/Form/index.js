9d85e48a159b59540eb8c4dc4afe9f2d53242ba0
Jhonatan Lucco <149596203+JhonatanHermes@users.noreply.github.com>
2025-01-09 15:22:33 -0300

    Add files via upload

---
+++ listadetarefas/src/components/Form/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FaPlus} from 'react-icons/fa';
+import './Form.css';
+
+export default function Form({handleChange, handleSubmit, novaTarefa}) {
+  return (
+    <form action="#" className="form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={novaTarefa}
+        onChange={handleChange}
+      />
+      <button type="submit">
+        <FaPlus />
+      </button>
+    </form>
+  )
+}
+
+Form.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  novaTarefa: PropTypes.string.isRequired,
+}
